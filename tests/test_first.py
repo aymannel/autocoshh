f35ca1e09c1bf6dd20f1559427534872a2e2d935
aelamrn1 <24988856+aymannel@users.noreply.github.com>
2022-07-23 09:56:26 +0100

    tests

---
+++ tests/test_first.py
@@ -0,0 +1,10 @@
+import unittest
+import autocoshh
+
+class TestSomething(unittest.TestCase):
+    def test_first(self):
+        self.assertEqual(5, 2+3)
+
+if __name__ == '__main__':
+    unittest.main()
+
