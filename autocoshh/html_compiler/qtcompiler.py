13cf3f43076595547deec243567f491547127bff
aelamrn1 <24988856+aymannel@users.noreply.github.com>
2022-08-24 03:19:16 +0100

    testing HTML compiler

---
+++ autocoshh/html_compiler/qtcompiler.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5 import QtCore, QtWidgets, QtWebEngineWidgets
+
+app = QtWidgets.QApplication(sys.argv)
+loader = QtWebEngineWidgets.QWebEngineView()
+loader.setZoomFactor(1)
+loader.page().pdfPrintingFinished.connect(
+    lambda *args: print('finished:', args))
+loader.load(QtCore.QUrl('https://en.wikipedia.org/wiki/Main_Page'))
+
+def emit_pdf(finished):
+    loader.show()
+    loader.page().printToPdf("test.pdf")
+
+loader.loadFinished.connect(emit_pdf)
+
+app.exec()
+
